409ff9e96ad875c0efaaf691d8e40ca14c0f58f3
Ammar Bandukwala <ammar@ammar.io>
2016-07-24 10:29:13 -0500

    hello vc

---
+++ README.md
@@ -0,0 +1,21 @@
+# String Cast Benchmarks for Golang
+
+While coding Go I commonly find the need to []byte(string) or string([]byte).
+These simple benchmarks demystify the performance impacts of using these operations in your code.
+
+
+## Usage
+```go test -bench=.```
+
+## My Results
+On my i5-5200U CPU @ 2.20GHz I got these results
+
+```
+BenchmarkTestStringByteLen10-4          30000000                56.1 ns/op
+BenchmarkTestStringByteLen100-4         20000000                92.9 ns/op
+BenchmarkTestStringByteLen1000-4        20000000                95.5 ns/op
+BenchmarkTestByteStringLen10-4          30000000                56.1 ns/op
+BenchmarkTestByteStringLen100-4         20000000                92.4 ns/op
+BenchmarkTestByteStringLen1000-4         3000000               389 ns/op
+ok      github.com/ammario/go-string-cast-benchmark     11.007s
+```
---
+++ string_cast_bench_test.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+var str10 = "1234567890"
+var byt10 = []byte(str10)
+
+var str100 string
+var byt100 []byte
+
+var str1000 string
+var byt1000 []byte
+
+func init() {
+	buf := &bytes.Buffer{}
+
+	for i := 0; i < 100; i++ {
+		buf.WriteByte('a')
+	}
+
+	str100, byt100 = buf.String(), []byte(buf.String())
+
+	buf.Reset()
+
+	for i := 0; i < 1000; i++ {
+		buf.WriteByte('a')
+	}
+	str1000, byt1000 = buf.String(), []byte(buf.String())
+
+	buf.Reset()
+}
+
+func benchmarkStringByte(b *testing.B, byt []byte) {
+	var str string //Some code to make sure our benchmark isn't optimized away
+	for i := 0; i < b.N; i++ {
+		str = string(byt)
+	}
+	_ = str
+}
+
+func benchmarkByteString(b *testing.B, str string) {
+	var by []byte
+	for i := 0; i < b.N; i++ {
+		by = []byte(str)
+	}
+	_ = by
+}
+
+func BenchmarkTestStringByteLen10(b *testing.B) {
+	benchmarkStringByte(b, byt10)
+}
+
+func BenchmarkTestStringByteLen100(b *testing.B) {
+	benchmarkStringByte(b, byt100)
+}
+
+func BenchmarkTestStringByteLen1000(b *testing.B) {
+	benchmarkStringByte(b, byt100)
+}
+
+func BenchmarkTestByteStringLen10(b *testing.B) {
+	benchmarkByteString(b, str10)
+}
+func BenchmarkTestByteStringLen100(b *testing.B) {
+	benchmarkByteString(b, str100)
+}
+func BenchmarkTestByteStringLen1000(b *testing.B) {
+	benchmarkByteString(b, str1000)
+}
